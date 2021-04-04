41f34e1eeb0689794d285e0ff27a43a9cdd2d4e6
Victor <gaeshi@gmail.com>
2021-04-04 18:50:35 +0900

    Add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,3 +19,15 @@ import { AppComponent } from './containers/app/app.component';
 })
 export class AppModule {
 }
+
+/*
+  var firebaseConfig = {
+    apiKey: "AIzaSyDOioJeydPRJr6eY6Y1EUJAb2XltmuUSyo",
+    authDomain: "fitness-app-e8205.firebaseapp.com",
+    databaseURL: "https://fitness-app-e8205-default-rtdb.firebaseio.com",
+    projectId: "fitness-app-e8205",
+    storageBucket: "fitness-app-e8205.appspot.com",
+    messagingSenderId: "788618193920",
+    appId: "1:788618193920:web:99d1b397ce8605d3237ce5"
+  };
+ */
